d20672157a4026b67acb28ec4de3bd2e8f76c07e
Henry Bryant <69921401+hcbryant9@users.noreply.github.com>
2022-10-02 22:34:23 -0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -6,17 +6,14 @@ import time
 from flask import Flask, jsonify, request 
 from flask_httpauth import HTTPDigestAuth as FlaskDigestAuth
 
+
+#https://pinghcb.herokuapp.com/
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret key here'
 auth = FlaskDigestAuth()
-
-pingpong_t = 14
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5001,debug=True)
 
 users = {
     "vcu":"rams"
@@ -28,15 +25,14 @@ def get_pw(username):
         return users.get(username)
     return None
 
-class ValidationError(ValueError):
-    pass
-
-@app.route('/users', methods=['GET'])
+@app.route('/ping', methods=['GET'])
 @auth.login_required
 def index():
-    response = requests.get(url="http://127.0.0.1:5000/users", auth = ('vcu','rams'))
+    
+    response = requests.get(url='http://127.0.0.1:5000/pong')
     return jsonify({
         'pingpong_t': str(response.elapsed.total_seconds()*1000)
     }),201
 
-
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=5001,debug=True)
